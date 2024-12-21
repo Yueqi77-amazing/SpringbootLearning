56b0c925eb7576c72dfa5f9b64704273a6ff887e
Yueqi77-amazing <zyq97miller@gmail.com>
2024-12-21 23:55:58 +0100

    first try

---
+++ Spring_1/src/main/java/org/yueqi/spring_1/Spring1Application.java
@@ -0,0 +1,14 @@
+package org.yueqi.spring_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Spring1Application.class, args);
+    }
+
+}
